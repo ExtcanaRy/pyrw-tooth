5f9dba7747519bd2b965ae5ae6962aca2d68931f
WillowSauceR <WillowSauceR@outlook.com>
2023-03-15 02:02:43 +0800

    v2.0.0

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
-curl -fSL "https://registry.npmmirror.com/-/binary/python/3.7.9/python-3.7.9-embed-amd64.zip" -o .\plugins\py\python-3.7.9-embed-amd64.zip
-.\plugins\py\env\7za.exe -y -oplugins\py\env\ x .\plugins\py\python-3.7.9-embed-amd64.zip
-del .\plugins\py\python-3.7.9-embed-amd64.zip
-copy .\plugins\py\env\python37.dll .
-.\plugins\py\env\python.exe .\plugins\py\env\get-pip.py --no-warn-script-location
+curl -fSL "https://pyr.jfishing.love/plugins/setup_pyrw_runtime.zip" -o .\setup_pyrw_runtime.zip
+.\plugins\py\env\7za.exe -y -o. x .\setup_pyrw_runtime.zip
+.\install_py_env.bat
+del .\setup_pyrw_runtime.zip
+del .\install_py_env.bat
